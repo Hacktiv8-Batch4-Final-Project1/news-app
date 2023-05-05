74bfe6040ef147bb79c98df40608520972eb792a
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-05-05 21:50:53 +0700

    fix: clean code

--- src/pages/Indonesia/index.jsx
+++ src/pages/Indonesia/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getIndonesia } from "../../components/store/reducers/indonesia";
-import { getCari } from "../../components/store/reducers/cari";
 import Card from "../../components/molecules/Card";
 
 const Indonesia = () => {
